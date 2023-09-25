9685aeb27c435f2719db41c216ee6807735fde7a
Prakash Bahadur Singh <prakashbahadursingh2@gmail.com>
2023-09-25 14:34:54 -0700

    resized logo, menu content adjusted with css

--- sac-nepal/src/Components/navbar.js
+++ sac-nepal/src/Components/navbar.js
@@ -4,16 +4,23 @@ import Logo from "../assets/logo.png";
 function navbar() {
   return (
     <div className="navbar">
-      <div className="leftSide">
-        <img src={Logo} alt="sacLogo" />
+      <div>
+        <h1 className="mainTitle">
+          Greater Sacramento Nepalese Community Center
+        </h1>
       </div>
-      <div className="rightSide">
-        <a href="/"> Home </a>
-        <a href="/events">Events</a>
-        <a href="/news">News</a>
-        <a href="/market">Market</a>
-        <a href="/membersCommittee">Members & Committee</a>
-        <a href="/aboutUs">About Us</a>
+      <div className="menuContent">
+        <div className="leftSide">
+          <img src={Logo} alt="sacLogo" width="150" height="150" />
+        </div>
+        <div className="rightSide">
+          <a href="/"> Home </a>
+          <a href="/events">Events</a>
+          <a href="/news">News</a>
+          <a href="/market">Market</a>
+          <a href="/membersCommittee">Members & Committee</a>
+          <a href="/aboutUs">About Us</a>
+        </div>
       </div>
     </div>
   );
