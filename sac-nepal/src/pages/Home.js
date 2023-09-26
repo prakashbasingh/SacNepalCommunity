d0a51b361899dca4ec64c2ac46238844aabbf86b
Prakash Bahadur Singh <prakashbahadursingh2@gmail.com>
2023-09-25 21:23:47 -0700

    footer content added plus home page skeleton

--- sac-nepal/src/pages/Home.js
+++ sac-nepal/src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "../cssFiles/home.css";
 
 function Home() {
   return (
     <div className="home">
-      <h1> Home, Hello World</h1>
+      <div className="addContainer1">Add Container</div>
+      <div className="mainContainer">
+        <h1> Home, Hello World</h1>
+      </div>
+      <div className="addContainer2">Add Container</div>
     </div>
   );
 }
