58d5ea658c64688037ab5f48a73841cfa6e0ff04
Prakash Bahadur Singh <prakashbahadursingh2@gmail.com>
2023-09-29 22:34:08 -0700

    Adjusting home Page with footer. now footer stick to the bottom of the page

--- sac-nepal/src/Components/footer.js
+++ sac-nepal/src/Components/footer.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "../cssFiles/footer.css";
 
+// date and time
+var today = new Date();
+var date =
+  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+var time =
+  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date + " " + time;
+//
+
 function footer() {
   return (
     <div className="footer">
@@ -12,23 +21,24 @@ function footer() {
         <a href="/membersCommittee">Members & Committee</a>
         <a href="/aboutUs">About Us</a>
       </div>
-      <div className="footerContainer">
-        <div className="footerMain">
-          <div>
-            <h4>Contact SNCC</h4>
-            <p>7700 Auburn Boulevard</p>
-            <p>Citrus Heights, CA 95610</p>
-            <p>Email: sacnepal2020@gmail.com</p>
-          </div>
-          <div>
-            <h4>Keep in touch</h4>
-            <form>
-              <label for="fname">your email here</label>
-              <br></br>
-              <input type="text" id="fname" name="fname"></input>
-              <button>Submit</button>
-            </form>
-          </div>
+      <div className="footerMain">
+        <div>
+          <h4>Contact SNCC</h4>
+          <p>7700 Auburn Boulevard</p>
+          <p>Citrus Heights, CA 95610</p>
+          <p>Email: sacnepal2020@gmail.com</p>
+        </div>
+        <div>
+          <h4>Keep in touch</h4>
+          <form>
+            <label for="fname">your email here</label>
+            <br></br>
+            <input type="text" id="fname" name="fname"></input>
+            <button>Submit</button>
+          </form>
+        </div>
+        <div>
+          <p>{dateTime}</p>
         </div>
       </div>
     </div>
--- sac-nepal/src/pages/Home.js
+++ sac-nepal/src/pages/Home.js
@@ -1,28 +1,77 @@
 import React from "react";
 import "../cssFiles/home.css";
+import Logo from "../assets/logo.png";
 
 function Home() {
   return (
     <div className="home">
-      <div className="addContainer1">Add Container</div>
-      <div className="mainContainer">
-        <div className="home4">
-          <h4>Our Agenda</h4>
-        </div>
-        <div className="home1">
-          <h4>What's happening in social media</h4>
-        </div>
-        <div className="home2">
-          <h4>Quick Peek at coming events</h4>
-        </div>
-        <div className="home3">
-          <h4>Whats on the Market</h4>{" "}
+      <div className="homeImageContainer">
+        <div>
+          <img src={Logo} alt="logo"></img>
         </div>
       </div>
-      <div className="addContainer2">Add Container</div>
+      <div className="homeContentContainer">
+        <div className="addContainer1">
+          <h4>Add Container 1</h4>
+          <div className="add1A"></div>
+          <div className="add1B"></div>
+          <div className="add1C"></div>
+        </div>
+        <div className="mainContainer">
+          <div className="home1">
+            <div className="ObjHeading">
+              <h4>Our Objectives</h4>
+            </div>
+            <div className="goalList">
+              <li>To establish a Nepalese Community Center.</li>
+              <li>
+                To operate a platform and organize Nepali families with social,
+                cultural, spiritual, national and international values on a
+                regular basis for the benefit of the community.
+              </li>
+              <li>
+                To establish and maintain a community center for the purpose of
+                conducting educational, cultural, literary, and performing arts
+                activities consistent with the mission as stated Article 1.4.
+              </li>
+              <li>
+                To encourage sponsoring and/or supporting community service and
+                charitable activities for the benefit of the community and the
+                nation as a whole.
+              </li>
+              <li>
+                To work with other community organizations on areas of common
+                interests
+              </li>
+            </div>
+          </div>
+          <div className="home2">
+            <h4>Quick Peek at upcoming events</h4>
+            <div className="home2Item">
+              <div>
+                <p>Stay tuned...... for upcoming event......</p>
+              </div>
+              <div>
+                <p>Checkout previous events</p>
+              </div>
+            </div>
+          </div>
+          <div className="home3">
+            <h4>Whats on the Market</h4>{" "}
+          </div>
+          <div className="home4">
+            <h4>What's happening in social media</h4>
+          </div>
+        </div>
+        <div className="addContainer2">
+          <h4>Add Container 2</h4>
+          <div className="add2A"></div>
+          <div className="add2B"></div>
+          <div className="add2C"></div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Home;
-//   style={{ border: "none", overflow: "hidden" }}
